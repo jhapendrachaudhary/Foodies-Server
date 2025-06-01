b7dd380c2c05d45db0eabfeaa8ef05a7d0064145
Jhapendra <theunknownmen777@gmail.com>
2025-06-01 21:42:52 +0545

    rtr

--- src/main/java/in/Jhapuu/foodiesapi/config/SecurityConfig.java
+++ src/main/java/in/Jhapuu/foodiesapi/config/SecurityConfig.java
@@ -63,7 +63,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:5174",
-                "https://online-foods-delivary.vercel.app/"
+                "https://online-foods-delivary.vercel.app/",
+                "https://wonderful-encouragement-production.up.railway.app"
 
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));

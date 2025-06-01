89f7f8809b1ab293baa631439a8f05bbd05e667e
Jhapendra <theunknownmen777@gmail.com>
2025-06-01 23:16:00 +0545

    version

--- src/main/java/in/Jhapuu/foodiesapi/FoodiesapiApplication.java
+++ src/main/java/in/Jhapuu/foodiesapi/FoodiesapiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodiesapiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Java version: " + System.getProperty("java.version"));
 		SpringApplication.run(FoodiesapiApplication.class, args);
 	}
 
